e31dace23c67c3486c4ea55df965bda12679b85e
leggetter <phil@leggetter.co.uk>
2016-02-02 23:17:44 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,66 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
+    "optional": [
+      "runtime",
+      "optimisation.modules.system"
+    ]
+  },
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "babel": "npm:babel-core@5.8.35",
+    "babel-runtime": "npm:babel-runtime@5.8.35",
+    "core-js": "npm:core-js@1.2.6",
+    "css": "github:systemjs/plugin-css@0.1.20",
+    "d3": "npm:d3@3.5.14",
+    "epoch-charting": "npm:epoch-charting@0.8.4",
+    "jquery": "npm:jquery@2.2.0",
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:babel-runtime@5.8.35": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:core-js@1.2.6": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:epoch-charting@0.8.4": {
+      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
+      "d3": "npm:d3@3.5.14"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    }
+  }
+});
---
+++ src/gauge.js
@@ -0,0 +1,154 @@
+import $ from 'jquery';
+import d3 from 'd3';
+import epoch from 'epoch-charting'
+import 'epoch-charting/dist/css/epoch.min.css!'
+
+class Activity {
+  constructor(name, aaarrrps) {
+    this.name = name;
+    this.aaarrrps = aaarrrps;
+  }
+}
+
+const Activities = [
+  new Activity('Writing docs', ['acquisition', 'activation', 'product']),
+  new Activity('Library dev', ['activation', 'product']),
+  new Activity('Quick start apps', ['activation', 'product']),
+  new Activity('Blog posts', ['awareness', 'acquisition', 'activation', 'retention']),
+  new Activity('Sponsor events', ['awareness', 'acquisition']),
+  new Activity('Give talks', ['awareness', 'acquisition']),
+  new Activity('Answer support queries', ['activation', 'retention', 'product']),
+  new Activity('Support pre-sales', ['acquisition', 'activation']),
+  new Activity('Dedicated forum', ['activation', 'retention']),
+  new Activity('Alpha/Beta programme', ['retention', 'product']),
+  new Activity('Office hours', ['activation', 'retention']),
+  new Activity('Capture developer feedback', ['retention', 'product']),
+  new Activity('Help with recruitment', ['awareness'])
+];
+
+class Gauge {
+  constructor() {
+    this.createActivities();
+    this._checkboxCount = Activities.length;
+    this._acquisitionCount = Activities.filter(a => {
+      return a.aaarrrps.indexOf('acquisition') != -1;
+    }).length;
+    
+    this.activityAAARRRPs = {};
+    Activities.forEach(act => {
+      act.aaarrrps.forEach(aaa => {
+        if(!this.activityAAARRRPs[aaa]) {
+          this.activityAAARRRPs[aaa] = 0;
+        }
+        ++this.activityAAARRRPs[aaa];
+      })
+    });
+    console.log(this.activityAAARRRPs);
+    
+    this._gauge = this.createGaugeChart();
+    this._pie = this.createPieChart();
+    
+    $('input[type=checkbox]').change(this.handleChecked.bind(this));
+  }
+  
+  createActivities() {
+    let activities = $('#activites');
+    Activities.forEach(activity => {
+      activities.append(`<li><label><input type="checkbox" data-label="${activity.name}" /> ${activity.name}</label></li>`);
+    });
+  }
+  
+  createGaugeChart() {
+    return $('#gaugeChart').epoch({
+      type: 'time.gauge',
+      value: 0,
+      format: function(v) {
+        var label = 'None';
+        if(v >= 0.5) {
+          label = "Advocacy";
+        }
+        else if(v > 0) {
+          label ='Evangelism';
+        }
+        
+        return label;
+      }
+    });
+  }
+  
+  createPieChart() {
+    return $('#pie').epoch({
+      type: 'pie',
+      data: []
+    });
+  }
+  
+  handleChecked() {
+    let checked = $('input[type=checkbox]:checked');
+    let checkedCount = checked.length;
+    
+    let aaarrrs = this.calcAaarrrCount(checked);
+    let pieData = [];
+    Object.keys(aaarrrs).forEach(name => {
+      pieData.push({label: `${name} (${aaarrrs[name]})`, value: aaarrrs[name]});
+    });
+    
+    // console.log(pieData);
+    this._pie.update(pieData);
+    
+    // if awareness and acquisition is a priority it's Evangelism
+    // if - in addition - product, activation and retention are hight it's Advocacy
+    let percentEvangelism = this.getPercentFor(aaarrrs, ['awareness', 'acquisition']);
+    let percentAdvocacy = this.getPercentFor(aaarrrs, ['product', 'activation', 'retention']);
+    
+    console.log('Evangelism:', percentEvangelism);
+    console.log('Advocacy:', percentAdvocacy);
+    
+    let fakeGaugePercent = 0.75; // hope for advocacy
+    if(percentEvangelism > percentAdvocacy) {
+      fakeGaugePercent = 0.25;
+    }
+    
+    if(fakeGaugePercent !== this._gauge.value) {
+      this._gauge.update(fakeGaugePercent);
+    }
+  }
+  
+  calcAaarrrCount(checked) {
+    let aaarrrs = {};
+    
+    checked.each((index, input) => {
+      let name = input.getAttribute('data-label');
+      let activity = Activities.find(a => {
+        return a.name === name;
+      });
+      
+      activity.aaarrrps.forEach(ar => {
+        if(!aaarrrs[ar]) {
+          aaarrrs[ar] = 0;
+        }
+        ++aaarrrs[ar];
+      })  
+    });
+    
+    return aaarrrs;
+  }
+  
+  getPercentFor(aaarrrs, checkFor) {
+    var total = 0;
+    Object.keys(aaarrrs).forEach(ar => {
+      total += aaarrrs[ar];
+    });
+    
+    let checkedTotal = 0;
+    checkFor.forEach(toCheck => {
+      if(aaarrrs[toCheck]) {
+        checkedTotal += aaarrrs[toCheck];
+      }
+    });
+    return parseFloat((checkedTotal/total).toFixed(2));
+  }
+  
+}
+
+new Gauge();
