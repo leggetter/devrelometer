8b28b56e2631ae25224e3be01f0e19a0c0885252
leggetter <phil@leggetter.co.uk>
2016-02-03 16:11:16 +0000

    Update descriptions

--- src/Activities.js
+++ src/Activities.js
@@ -1,18 +1,18 @@
 import Activity from './Activity';
 
 const Activities = [
-  new Activity('Writing docs', ['acquisition', 'activation', 'product']),
-  new Activity('Library dev', ['activation', 'product']),
-  new Activity('Quick start apps', ['activation', 'product']),
-  new Activity('Blog posts', ['awareness', 'acquisition', 'activation', 'retention']),
-  new Activity('Webinars', ['awareness', 'acquisition', 'activation', 'retention']),
+  new Activity('Write documenation', ['acquisition', 'activation', 'product']),
+  new Activity('Developer libraries', ['activation', 'product']),
+  new Activity('Create Quickstart apps', ['activation', 'product']),
+  new Activity('Write Blog posts', ['awareness', 'acquisition', 'activation', 'retention']),
+  new Activity('Run Webinars', ['awareness', 'acquisition', 'activation', 'retention']),
   new Activity('Sponsor events', ['awareness', 'acquisition']),
   new Activity('Give talks', ['awareness', 'acquisition']),
   new Activity('Answer support queries', ['activation', 'retention', 'product']),
   new Activity('Support pre-sales', ['acquisition', 'activation']),
-  new Activity('Dedicated forum', ['activation', 'retention']),
-  new Activity('Alpha/Beta programme', ['retention', 'product']),
-  new Activity('Office hours', ['activation', 'retention']),
+  new Activity('Monitor dedicated forum', ['activation', 'retention']),
+  new Activity('Run Alpha/Beta programme', ['retention', 'product']),
+  new Activity('Run office hours', ['activation', 'retention']),
   new Activity('Capture developer feedback', ['retention', 'product']),
   new Activity('Help with recruitment', ['awareness'])
 ];
